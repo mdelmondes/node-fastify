fa81d59d6632034dfb742e602140e857301761bf
Matheus Delmondes <mdelmondes@icloud.com>
2023-09-12 00:08:50 -0300

    Api com node fastify

---
+++ server.js
@@ -0,0 +1,52 @@
+//import { DatabaseMemory } from "./database-memory.js"
+import { DatabasePostgres } from "./database-postgres.js"
+import { fastify } from "fastify"
+
+
+const database = new DatabasePostgres()
+
+const server = fastify()
+
+server.post('/videos', async (req, res) => {
+    const {title, description, duration} = req.body
+    
+    await database.create({
+        title,
+        description,
+        duration,
+    })
+
+    return res.status(201).send()
+})
+
+server.get('/videos', async (req, res) => {
+    const search = req.query.search
+    const videos = await database.list(search)
+
+    return videos
+})
+
+server.put('/videos/:id', async (req, res) => {
+    const {title, description, duration} = req.body
+    const videoId = req.params.id
+
+    await database.update(videoId, {
+        title,
+        description,
+        duration,
+    })
+
+    return res.status(204).send()
+})
+
+server.delete('/videos/:id', async (req, res) => {
+    const videoId = req.params.id
+
+    await database.delete(videoId)
+
+    return res.status(204).send()
+})
+
+server.listen({
+    port: process.env.PORT ?? 3333
+})
